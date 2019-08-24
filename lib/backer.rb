3a41e43bac29bb73705967d1adcac0641d893454
Angie Goza <angielg9888@gmail.com>
2019-08-24 18:21:12 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Backer
+  attr_reader :name, :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(title)
+    @backed_projects << title
+    
+  end
+end
+
+#this Backer needs to go into the Project @backers array from within #back_project
+# @backers << name
+#how do I access @backers?
---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+'require pry'
+class Project
+  attr_reader :title, :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+    backers = @backers
+  end
+  
+  def add_backer(name)
+    @backers << name
+    
+  end
+end
